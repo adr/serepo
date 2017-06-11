dd154ba1d362d69394d19956ecebacdf41eaa7a8
Andreas <abuechle@hsr.ch>
2017-06-11 14:24:17 +0200

    Add checkbox to control if commit contains SE-Items which represents packages/folders.

--- serepo-client-webapp/src/main/java/ch/hsr/isf/serepo/client/webapp/view/seitems/containers/seitem/SeItemTreeContainer.java
+++ serepo-client-webapp/src/main/java/ch/hsr/isf/serepo/client/webapp/view/seitems/containers/seitem/SeItemTreeContainer.java
@@ -12,109 +12,129 @@ import com.vaadin.event.ItemClickEvent;
 import com.vaadin.event.ItemClickEvent.ItemClickListener;
 import com.vaadin.server.FontAwesome;
 import com.vaadin.ui.AbstractSelect.ItemCaptionMode;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.CheckBox;
 import com.vaadin.ui.CustomComponent;
 import com.vaadin.ui.Panel;
 import com.vaadin.ui.Tree;
+import com.vaadin.ui.VerticalLayout;
 
 import ch.hsr.isf.serepo.data.restinterface.seitem.SeItem;
 
 public class SeItemTreeContainer extends CustomComponent {
-	
-	private static final long serialVersionUID = -3629755455435817506L;
 
-	public interface Listener {
-		void seItemClicked(SeItemTreeItem seItemTreeItem);
-	}
+  private static final long serialVersionUID = -3629755455435817506L;
 
-	private final Tree tree;
-	private final HierarchicalContainer container = new HierarchicalContainer();
-	private Map<String, SeItemTreeItem> pathToSeTreeItem = new HashMap<String, SeItemTreeItem>();
-	private Listener listener;
-	
-	public SeItemTreeContainer() {
+  public interface Listener {
+    void seItemClicked(SeItemTreeItem seItemTreeItem);
+  }
 
-		setSizeFull();
-		
-		tree = new Tree(null, container);
-		tree.setSizeUndefined();
-		tree.setSelectable(true);
-		tree.setNullSelectionAllowed(false);
-		tree.setItemCaptionMode(ItemCaptionMode.ID_TOSTRING);
-		tree.addItemClickListener(new ItemClickListener() {
-			private static final long serialVersionUID = -7738562408316085806L;
+  private final Tree tree;
+  private final HierarchicalContainer container = new HierarchicalContainer();
+  private Map<String, SeItemTreeItem> pathToSeTreeItem = new HashMap<String, SeItemTreeItem>();
+  private CheckBox chbxSeItemCanRepresentFolder = new CheckBox("Commit contains SE-Items which represents packages/folders", true);
+  private Listener listener;
 
-			@Override
-			public void itemClick(ItemClickEvent event) {
-				Object itemId = event.getItemId();
-				if (tree.hasChildren(itemId)) {
-					if (tree.isExpanded(itemId)) {
-						tree.collapseItem(itemId);
-					} else {
-						tree.expandItem(itemId);
-					}
-				}
-			}
-		});
-		tree.addValueChangeListener(new ValueChangeListener() {
-			private static final long serialVersionUID = -5912075834762514167L;
+  private List<SeItem> seItems;
 
-			@Override
-			public void valueChange(ValueChangeEvent event) {
-			  if (event.getProperty().getValue() != null) {
-			    if (SeItemTreeItem.class == event.getProperty().getValue().getClass()) {
-			      if (listener != null) {
-			        listener.seItemClicked(((SeItemTreeItem) event.getProperty().getValue()));
-			      }
-			    }
-			  }
-			}
-		});
-		
-		Panel panel = new Panel(tree);
-		panel.setSizeFull();
+  public SeItemTreeContainer() {
 
-		setCompositionRoot(panel);
-		
-	}
+    setSizeFull();
 
-	public void setListener(Listener listener) {
-		this.listener = listener;
-	}
+    tree = new Tree(null, container);
+    tree.setSizeUndefined();
+    tree.setSelectable(true);
+    tree.setNullSelectionAllowed(false);
+    tree.setItemCaptionMode(ItemCaptionMode.ID_TOSTRING);
+    tree.addItemClickListener(new ItemClickListener() {
+      private static final long serialVersionUID = -7738562408316085806L;
 
-	public void setSeItems(List<SeItem> seItems) {
-		container.removeAllItems();
-		pathToSeTreeItem.clear();
+      @Override
+      public void itemClick(ItemClickEvent event) {
+        Object itemId = event.getItemId();
+        if (tree.hasChildren(itemId)) {
+          if (tree.isExpanded(itemId)) {
+            tree.collapseItem(itemId);
+          } else {
+            tree.expandItem(itemId);
+          }
+        }
+      }
+    });
+    tree.addValueChangeListener(new ValueChangeListener() {
+      private static final long serialVersionUID = -5912075834762514167L;
 
-		for (SeItem seItem : seItems) {
-		  LinkedList<TreeItem> folders = createFolders(seItem);
-		  
-		  SeItemTreeItem seItemTreeItem = new SeItemTreeItem(seItem);
-		  container.addItem(seItemTreeItem);
-		  container.setParent(seItemTreeItem, folders.peekLast());
-		  container.setChildrenAllowed(seItemTreeItem, false);
-		  tree.setItemIcon(seItemTreeItem, FontAwesome.FILE_O);
-		  pathToSeTreeItem.put(seItem.getFolder() + seItem.getName(), seItemTreeItem);
-		  
-		}
-		
-	}
-	
-	private LinkedList<TreeItem> createFolders(SeItem seItem) {
-	  LinkedList<TreeItem> listFolderTreeItems = new LinkedList<>();
-	  TreeItem currentParent = null;
-	  String[] folders = seItem.getFolder().split("/");
-	  StringBuilder currentFolderPath = new StringBuilder();
+      @Override
+      public void valueChange(ValueChangeEvent event) {
+        if (event.getProperty().getValue() != null) {
+          if (SeItemTreeItem.class == event.getProperty().getValue().getClass()) {
+            if (listener != null) {
+              listener.seItemClicked(((SeItemTreeItem) event.getProperty().getValue()));
+            }
+          }
+        }
+      }
+    });
 
-	  String folderDelimiter = "";
-      for (String folder : folders) {
-        currentFolderPath.append(folderDelimiter).append(folder);
-        
+    Panel panel = new Panel(tree);
+    panel.setSizeFull();
+
+    VerticalLayout vlPanel = new VerticalLayout(panel, chbxSeItemCanRepresentFolder);
+    vlPanel.setSizeFull();
+    vlPanel.setSpacing(true);
+    vlPanel.setExpandRatio(panel, 1f);
+    vlPanel.setComponentAlignment(chbxSeItemCanRepresentFolder, Alignment.BOTTOM_LEFT);
+
+    setCompositionRoot(vlPanel);
+
+    chbxSeItemCanRepresentFolder.addValueChangeListener(new ValueChangeListener() {
+      private static final long serialVersionUID = 7940276200234018080L;
+
+      @Override
+      public void valueChange(ValueChangeEvent event) {
+        setSeItems(seItems);
+      }
+    });
+
+  }
+
+  public void setListener(Listener listener) {
+    this.listener = listener;
+  }
+
+  public void setSeItems(List<SeItem> seItems) {
+    this.seItems = seItems;
+    container.removeAllItems();
+    pathToSeTreeItem.clear();
+
+    for (SeItem seItem : seItems) {
+      LinkedList<TreeItem> folders = createFolders(seItem);
+
+      SeItemTreeItem seItemTreeItem = new SeItemTreeItem(seItem);
+      container.addItem(seItemTreeItem);
+      container.setParent(seItemTreeItem, folders.peekLast());
+      container.setChildrenAllowed(seItemTreeItem, false);
+      tree.setItemIcon(seItemTreeItem, FontAwesome.FILE_O);
+      pathToSeTreeItem.put(seItem.getFolder() + seItem.getName(), seItemTreeItem);
+
+    }
+
+  }
+
+  private LinkedList<TreeItem> createFolders(SeItem seItem) {
+    LinkedList<TreeItem> listFolderTreeItems = new LinkedList<>();
+    TreeItem currentParent = null;
+    String[] folders = seItem.getFolder().split("/");
+    StringBuilder currentFolderPath = new StringBuilder();
+
+    String folderDelimiter = "";
+    for (String folder : folders) {
+      currentFolderPath.append(folderDelimiter).append(folder);
+
+      if (chbxSeItemCanRepresentFolder.getValue()) {
         if (!pathToSeTreeItem.containsKey(currentFolderPath.toString())) {
           // there is no SE-Item which represents current folder!
-          FolderTreeItem folderTreeItem = new FolderTreeItem(currentFolderPath.toString(), folder, currentFolderPath.toString());
-          container.addItem(folderTreeItem);
-          container.setParent(folderTreeItem, currentParent);
-          tree.setItemIcon(folderTreeItem, FontAwesome.FOLDER_O);
+          FolderTreeItem folderTreeItem = createFolderTreeitem(currentParent, currentFolderPath, folder);
           currentParent = folderTreeItem;
           listFolderTreeItems.add(folderTreeItem);
         } else {
@@ -126,12 +146,25 @@ public class SeItemTreeContainer extends CustomComponent {
           currentParent = seItemTreeItem; // set SE-ItemTreeItem as parent!
           listFolderTreeItems.add(seItemTreeItem);
         }
-        
-        if (folderDelimiter.isEmpty()) {
-          folderDelimiter = "/";
-        }
+      } else {
+        FolderTreeItem folderTreeItem = createFolderTreeitem(currentParent, currentFolderPath, folder);
+        currentParent = folderTreeItem;
+        listFolderTreeItems.add(folderTreeItem);
       }
-      return listFolderTreeItems;
-	}
-	
+
+      if (folderDelimiter.isEmpty()) {
+        folderDelimiter = "/";
+      }
+    }
+    return listFolderTreeItems;
+  }
+
+  private FolderTreeItem createFolderTreeitem(TreeItem currentParent, StringBuilder currentFolderPath, String folder) {
+    FolderTreeItem folderTreeItem = new FolderTreeItem(currentFolderPath.toString(), folder, currentFolderPath.toString());
+    container.addItem(folderTreeItem);
+    container.setParent(folderTreeItem, currentParent);
+    tree.setItemIcon(folderTreeItem, FontAwesome.FOLDER_O);
+    return folderTreeItem;
+  }
+
 }
