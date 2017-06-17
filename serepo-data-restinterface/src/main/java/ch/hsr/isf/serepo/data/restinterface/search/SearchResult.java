13c0cad8a2135011a5326b228881efd14aa21a2a
Andreas <abuechle@hsr.ch>
2017-06-17 21:19:12 +0200

    Update search RESTful API.

--- serepo-data-restinterface/src/main/java/ch/hsr/isf/serepo/data/restinterface/search/SearchResult.java
+++ serepo-data-restinterface/src/main/java/ch/hsr/isf/serepo/data/restinterface/search/SearchResult.java
@@ -2,35 +2,43 @@ package ch.hsr.isf.serepo.data.restinterface.search;
 
 public class SearchResult {
 
-	private String repository;
-	private String commitId;
-	private String seItemUri;
-	
-	public SearchResult() {
-	}
+  private String repository;
+  private String commitId;
+  private String seItemUri;
+  private String seItemName;
 
-	public String getRepository() {
-		return repository;
-	}
+  public SearchResult() {}
 
-	public void setRepository(String repository) {
-		this.repository = repository;
-	}
+  public String getRepository() {
+    return repository;
+  }
 
-	public String getCommitId() {
-		return commitId;
-	}
+  public void setRepository(String repository) {
+    this.repository = repository;
+  }
 
-	public void setCommitId(String commitId) {
-		this.commitId = commitId;
-	}
+  public String getCommitId() {
+    return commitId;
+  }
 
-	public String getSeItemUri() {
-		return seItemUri;
-	}
+  public void setCommitId(String commitId) {
+    this.commitId = commitId;
+  }
 
-	public void setSeItemUri(String seItemUri) {
-		this.seItemUri = seItemUri;
-	}
+  public String getSeItemUri() {
+    return seItemUri;
+  }
+
+  public void setSeItemUri(String seItemUri) {
+    this.seItemUri = seItemUri;
+  }
+
+  public String getSeItemName() {
+    return seItemName;
+  }
+
+  public void setSeItemName(String seItemName) {
+    this.seItemName = seItemName;
+  }
 
 }
