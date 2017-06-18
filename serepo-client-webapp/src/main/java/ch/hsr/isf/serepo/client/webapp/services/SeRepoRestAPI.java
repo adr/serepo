d411fcb0ed6da8f1639b8cc608b82c6875d01e31
Andreas <abuechle@hsr.ch>
2017-06-18 14:51:08 +0200

    Improve RESTful HTTP API helper class.

--- serepo-client-webapp/src/main/java/ch/hsr/isf/serepo/client/webapp/services/SeRepoRestAPI.java
+++ serepo-client-webapp/src/main/java/ch/hsr/isf/serepo/client/webapp/services/SeRepoRestAPI.java
@@ -5,40 +5,78 @@ import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import ch.hsr.isf.serepo.client.webapp.model.Settings;
 import ch.hsr.isf.serepo.data.restinterface.commit.CommitContainer;
 import ch.hsr.isf.serepo.data.restinterface.repository.RepositoryContainer;
+import ch.hsr.isf.serepo.data.restinterface.search.SearchContainer;
 
 public class SeRepoRestAPI {
+  
+  public static class RestfulApiException extends RuntimeException {
+    private static final long serialVersionUID = 7815841713880704891L;
+
+    public RestfulApiException(String message) {
+      super(message);
+    }
+
+    public RestfulApiException(String message, Throwable cause) {
+      super(message, cause);
+    }
+  }
 
   private SeRepoRestAPI() {};
   
   public static RepositoryContainer getRepositories() {
     String uri = String.format("%s/repos", Settings.getFromSession().getSerepoUrl());
-    return request(RepositoryContainer.class, uri);
+    return readEntity(RepositoryContainer.class, uri);
   }
 
   public static CommitContainer getCommitsForRepository(String repository) {
     String uri = String.format("%s/repos/%s/commits", Settings.getFromSession().getSerepoUrl(), repository);
-    return request(CommitContainer.class, uri);
+    return readEntity(CommitContainer.class, uri);
   }
   
-  private static <T> T request(Class<T> type, String uri) {
+  public static SearchContainer search(String query) {
+    String uri = String.format("%s/search", Settings.getFromSession().getSerepoUrl());
+    WebTarget target = createTarget(uri).queryParam("q", query);
+    return readEntity(SearchContainer.class, target);
+  }
+  
+  private static <T> T readEntity(Class<T> type, String uri) {
+    return readEntity(type, createTarget(uri));
+  }
+
+  private static <T> T readEntity(Class<T> type, WebTarget target) {
+    Response respone = getResponse(target);
+    try {
+      if (respone.getStatus() == Status.OK.getStatusCode()) {
+        return respone.readEntity(type);
+      } else {
+        String msg;
+        try {
+          msg = respone.readEntity(String.class);
+        } catch (Throwable e) {
+          msg = "RESTful HTTP API error!";
+        }
+        throw new RestfulApiException(msg);
+      }
+    } finally {
+      respone.close();
+    }
+  }
+  
+  private static Response getResponse(WebTarget target) {
+    return target.request()
+                 .accept(MediaType.APPLICATION_JSON_TYPE)
+                 .get();
+  }
+
+  private static WebTarget createTarget(String uri) {
     Client client = ClientBuilder.newClient();
     WebTarget target = client.target(uri);
-    
-    Response response = null;
-    try {
-      response = target.request()
-                       .accept(MediaType.APPLICATION_JSON_TYPE)
-                       .get();
-      return response.readEntity(type);
-    } finally {
-      if (response != null) {
-        response.close();
-      }
-    }
+    return target;
   }
   
 }
